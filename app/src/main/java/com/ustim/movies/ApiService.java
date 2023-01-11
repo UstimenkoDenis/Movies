ae69dac86eef5ffe792e1ad223e47ed2a035dcec
Ustim <ustimenkodenis@mail.ru>
2023-01-11 21:50:08 +0300

    feat: Add MainViewModel

--- app/src/main/java/com/ustim/movies/ApiService.java
+++ app/src/main/java/com/ustim/movies/ApiService.java
@@ -2,10 +2,11 @@ package com.ustim.movies;
 
 import io.reactivex.rxjava3.core.Single;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface ApiService {
 
-    @GET("movie?token=E0M4KS3-D6NMC93-HP2S1RF-BSSKKQG&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&page=2&limit=5")
-    Single<MovieResponse> loadMovies();
+    @GET("movie?token=E0M4KS3-D6NMC93-HP2S1RF-BSSKKQG&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&limit=5")
+    Single<MovieResponse> loadMovies(@Query("page") int page);
 
 }
