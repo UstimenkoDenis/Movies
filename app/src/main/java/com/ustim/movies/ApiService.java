8b87ac07999b7ccc24b2a10eafd03df8ba5aeb32
Ustim <ustimenkodenis@mail.ru>
2023-01-16 00:52:02 +0300

    feat: trailers load

--- app/src/main/java/com/ustim/movies/ApiService.java
+++ app/src/main/java/com/ustim/movies/ApiService.java
@@ -9,4 +9,7 @@ public interface ApiService {
     @GET("movie?token=E0M4KS3-D6NMC93-HP2S1RF-BSSKKQG&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&limit=30")
     Single<MovieResponse> loadMovies(@Query("page") int page);
 
+    @GET("movie?token=E0M4KS3-D6NMC93-HP2S1RF-BSSKKQG&field=id")
+    Single<TrailerResponse> loadTrailers(@Query("search") int id);
+
 }
